6d38fb3f862bfc41678b8b962003d8883a9a7758
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2021-01-19 15:32:07 +0100

    dodałem propType Error

--- digema/src/Error.js
+++ digema/src/Error.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Error = (props) => {
   return (
@@ -7,11 +8,15 @@ const Error = (props) => {
         // <p style={{ color: "green" }}>{props.info}</p>
         <p style={{ color: "red" }}>{props.info}</p>
       ) : (
-        // <p style={{ color: "red" }}>{props.info}</p>
-        <span></span>
-      )}
+          // <p style={{ color: "red" }}>{props.info}</p>
+          <span></span>
+        )}
     </div>
   );
 };
 
+Error.propTypes = {
+  status: PropTypes.bool.isRequired,
+  info: PropTypes.string.isRequired,
+};
 export default Error;
